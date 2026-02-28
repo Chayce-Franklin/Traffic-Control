ea4a4624970adc05cb9d65bc33f34c9033808330
Chayce-Franklin1 <caf5989@psu.edu>
2026-02-28 13:48:31 -0500

    Create Code

---
+++ Code/Code.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd
+
+df = pd.read_csv("/Users/chaycefranklin/Desktop/GitHub/Traffic-Control/temp/traffic-prediction-dataset.csv")
+# print(df.describe)
+# print(df.head())
+print(df.columns)
