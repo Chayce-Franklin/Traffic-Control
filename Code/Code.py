52712dc29ccfcd3826ae3eb421d48ca82bdc7d15
Chayce-Franklin1 <caf5989@psu.edu>
2026-03-18 18:35:37 -0400

    Update

--- Code/Code.py
+++ Code/Code.py
@@ -1,7 +1,21 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 
-df = pd.read_csv("/Users/chaycefranklin/Desktop/GitHub/Traffic-Control/temp/traffic-prediction-dataset.csv")
-# print(df.describe)
-# print(df.head())
-print(df.columns)
+data = pd.read_csv("traffic-prediction-dataset.csv")
+print(data.describe)
+print(data.head())
+print(data.columns)
+
+## Data Preprocessing 
+from sklearn.model_selection import train_test_split
+
+# Split Data into train and test
+
+x_train, x_test, y_train, y_test = train_test_split(data[data.columns], test_size=0.2, random_state=0) 
+
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler()
+print(scaler)
